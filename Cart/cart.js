75775bd4bfc323550ec54c5a2449ee51b06665c0
Manish-XD <manishsharma.it1@bvp.edu.in>
2022-02-28 17:53:01 +0530

    added functionality in cart

--- Cart/cart.js
+++ Cart/cart.js
@@ -7,8 +7,66 @@ document.getElementById("next2").addEventListener("click", function () {
   document.querySelector(".card_1").style.display = "none";
   document.querySelector(".card_2").style.display = "none";
   document.querySelector(".payemnt-mode").style.display = "block ";
-  document.querySelectorAll(".payemnt-mode").forEach(function(b){
+  document.querySelectorAll(".payemnt-mode").forEach(function (b) {
     b.style.display = "flex";
-});
+  });
   document.querySelector(".card-3").style.display = "block";
 });
+
+// console.log(document.querySelector("#item_no1"));
+
+var i = 0;
+
+document.getElementById("minus1").addEventListener("click", function () {
+  i = i - 1;
+  document.getElementById("item_no1").innerHTML = i;
+  document.getElementById("burger").innerHTML = i*60;
+  document.getElementById("sub-total").innerHTML = i*60 + j*60 + k*60;
+  document.getElementById("total").innerHTML = (i*60 + j*60 + k*60) + 40;
+});
+
+document.getElementById("plus1").addEventListener("click", function () {
+  i = i + 1;
+  document.getElementById("item_no1").innerHTML = i;
+  document.getElementById("burger").innerHTML = i*60;
+  document.getElementById("sub-total").innerHTML = i*60 + j*60 + k*60;
+  document.getElementById("total").innerHTML = (i*60 + j*60 + k*60) + 40;
+});
+document.getElementById("item_no1").innerHTML = i;
+var j = 0;
+document.getElementById("minus2").addEventListener("click", function () {
+  j = j - 1;
+  document.getElementById("item_no2").innerHTML = j;
+  document.getElementById("pizza").innerHTML = j*60;
+  document.getElementById("sub-total").innerHTML = i*60 + j*60 + k*60;
+  document.getElementById("total").innerHTML = (i*60 + j*60 + k*60) + 40;
+});
+
+document.getElementById("plus2").addEventListener("click", function () {
+  j = j + 1;
+  document.getElementById("item_no2").innerHTML = j;
+  document.getElementById("pizza").innerHTML = j*60;
+  document.getElementById("sub-total").innerHTML = i*60 + j*60 + k*60;
+  document.getElementById("total").innerHTML = (i*60 + j*60 + k*60) + 40;
+});
+document.getElementById("item_no2").innerHTML = j;
+var k = 0;
+document.getElementById("minus3").addEventListener("click", function () {
+  k = k - 1;
+  document.getElementById("item_no3").innerHTML = k;
+  document.getElementById("biryani").innerHTML = k*60;
+  document.getElementById("sub-total").innerHTML = i*60 + j*60 + k*60;
+  document.getElementById("total").innerHTML = (i*60 + j*60 + k*60) + 40;
+});
+
+document.getElementById("plus3").addEventListener("click", function () {
+  k = k + 1;
+  document.getElementById("item_no3").innerHTML = k;
+  document.getElementById("biryani").innerHTML = k*60;
+  document.getElementById("sub-total").innerHTML = i*60 + j*60 + k*60;
+  document.getElementById("total").innerHTML = (i*60 + j*60 + k*60) + 40;
+});
+document.getElementById("item_no3").innerHTML = k;
+document.getElementById("burger").innerHTML = i;
+document.getElementById("pizza").innerHTML = j;
+document.getElementById("biryani").innerHTML = k;
